160d2d4655ed54df3b641068b2411e0098ea9708
Kasonsx <sxk644653293@163.com>
2017-01-04 16:49:06 +0800

    add orm.py

---
+++ www/orm.py
@@ -0,0 +1,178 @@
+# -*- coding:utf-8 -*-
+import aiomysql
+import logging，asyncio
+
+def log(sql,args=()):
+	logging.info('SQL:%s' % sql)
+# =================数据库操作======================
+# 创建连接池（全局）
+# 每个HTTP请求都可以直接从连接池中直接获取数据库连接
+# 好处：不必频繁地打开和关闭数据库连接，而是能复用就尽量复用
+async def create_pool(loop, **kw):
+	logging.info('Create database connection pool...')
+	global __pool
+	# 连接池由全局变量__pool存储
+	# 缺省情况下将编码设置为utf8，自动提交事务
+	__pool = await aiomysql.create_pool(
+		host = kw.get('host','localhost'),
+		port = kw.get('port', 3306),
+		user = kw['user'],
+		password = kw['password'],
+		db = kw['db'],
+		charset = kw.get('charset','utf8'),
+		autocommit = kw.get('autocommit',True),
+		maxsize = kw.get('maxsize',10),
+		minsize = kw.get('minsize',1),
+		loop=loop
+		)
+
+# SELECT操作
+async def select(sql,args,size=None):
+	log(sql,args)
+	global __pool
+	with (await __pool) as conn:
+		cur = await conn.cursor(aiomysql.DictCursor)
+		# SQL语句的占位符是?，而MySQL的占位符是%s，替换
+		await cur.execute(sql.replace('?','%s'), args or ())
+		if size:
+			# 获取最多指定数量的记录
+			rs = await cur.fetchmany(size)
+		else:
+			# 获取所有记录
+			rs = await cur.fetchall()
+		await cur.close()
+		logging.info('rows returned: %s' % len(rs))
+		return rs
+# Insert,Update,Delete操作
+async def execute(sql,args):
+	log(sql)
+	with (await __pool) as conn:
+		try:
+			cur = await conn.cursor()
+			await cur.execute(sql.replace('?','%s'),args)
+			affected = cur.rowcount
+			await cur.close()
+		except BaseException as e:
+			raise
+		return affected
+
+# ===================ORM===========================
+from orm import Model, StringField, IntegerField
+class Field(object):
+	def __init__(self, name, colume_type, primary_key, default):
+		self.name = name
+		self.colume_type = colume_type
+		self.primary_key = primary_key
+		self.default = default
+
+	def __str__(self):
+		return '<%s,%s:%s>' %(self.__class__.__name__, self.colume_type, self.name)
+# 映射varchar的StringField
+class StringField(Field):
+	def __init__(self, name=None, primary_key=False, default=None,ddl='varchar(100)'):
+		super().__init__(name,ddl,primary_key,default)
+
+# 映射信息
+class ModelMetaclass(type):
+	def __new__(cls, name, bases, attrs):
+		# 排除Model本身
+		if name =='Model':
+			return type.__new__(cls, name, bases, attrs)
+		# 获取table名称
+		tableName = attrs.get('__table__',None) or name
+		logging.info('found model:%s (table:%s)' %(name,tableName))
+		# 获取所有Field和主键名
+		mappings = dict()
+		fields = []
+		primaryKey = None
+		for k,v in attrs.items():
+			if isinstance(v, Field):
+				logging.info('  found mapping: %s ==> %s' %(k,v))
+				mappings[k] = v
+				if v.primary_key:
+					# 找到主键
+					if primaryKey:
+						raise RuntimeError('Duplicate primary key for field: %s' % k)
+					primaryKey = k
+				else:
+					fields.append(k)
+		if not primaryKey:
+			raise RuntimeError('Primary key not found.')
+		for k in mappings.keys():
+			attrs.pop(k)
+		escaped_fields = list(map(lambda f:'`%s`' %f, fields))
+		attrs['__mappings__'] = mappings # 保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey # 主键属性名
+        attrs['__fields__'] = fields # 除主键外的属性名
+        # 构造默认的SELECT, INSERT, UPDATE和DELETE语句:
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
+
+
+
+# 定义所有ORM映射的基类Model
+# 继承dict，实现__getattr__和__setattr__方法可以直接↓
+# >>> user['id']
+# 123
+# >>> user.id
+# 123
+class Model(dict, metaclass=ModelMetaclass):
+	def __init__(self, **kw):
+		super(Model,self).__init__(**kw)
+
+	def __getattr__(self,key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Model' object has no attribute '%s'" %key)
+
+	def __setattr__(self,key,value):
+		self[key] = value
+
+	def getValue(self,key):
+		return getattr(self,key,None)
+
+	def getValueOrDefault(self, key):
+		value = getattr(self,key,None)
+		if value is None:
+			field = self.__mappings__[key]
+			if field.default is not None:
+				value = field.default() if callable(field.default) else field.default
+				logging.debug('using default value for %s:%s' %(key,str(value)))
+				setattr(self,key,value)
+		return value
+
+	@classmethod
+	async def find(cls, pk):
+		' find object by primary key. ' 
+		rs = await select('%s where `%s`=?' %(cls.__select__, cls.__primary_key__),[pk], 1)
+		if len(rs) == 0:
+			return None
+		return cls(**rs[0])
+
+	async def save(self):
+		args = list(map(self.getValueOrDefault, self.__fields__))
+		srgs.append(self.getValueOrDefault(self.__primary_key__))
+		rows = await execute(self.__insert__, args)
+		if rows != 1:
+			logging.warn('failed to insert record:affected rows:%s' % rows)
+	#把一个User实例存入数据库
+	# user = User(id=123, name='Michael')
+	# yield from user.save()
+
+class User(Model):
+	__table__ = 'users'
+	id = IntegerField(primary_key=True)
+	name = StringField()
+	# id和name是User类的属性，不是实例的属性
+# 创建实例:
+# user = User(id=123, name='Michael')
+# 存入数据库:
+# user.insert()
+# 查询所有User对象:
+# users = User.findAll()
+
